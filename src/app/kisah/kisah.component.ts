ea2a2dc44f67ee3fe70d91657d9774ec87bbb4f9
Thomas Setiabudi <thomsetiabudi@gmail.com>
2018-05-01 09:08:27 +0700

    halaman kisah (in progress)
    halaman kisah (in progress)

--- src/app/kisah/kisah.component.ts
+++ src/app/kisah/kisah.component.ts
@@ -3,13 +3,59 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kisah',
   templateUrl: './kisah.component.html',
-  styleUrls: ['./kisah.component.scss']
+  styleUrls: [
+    './kisah.component.scss',
+    './kisah.component.css',
+    '../app.component.css'
+  ]
 })
 export class KisahComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    startTime();
   }
 
+}
+
+var weekday = new Array(7);
+weekday[0] = "Minggu";
+weekday[1] = "Senin";
+weekday[2] = "Selasa";
+weekday[3] = "Rabu";
+weekday[4] = "Kamis";
+weekday[5] = "Jumat";
+weekday[6] = "Sabtu";
+
+var month = new Array(7);
+month[0] = "Januari";
+month[1] = "Februari";
+month[2] = "Maret";
+month[3] = "April";
+month[4] = "Mei";
+month[5] = "Juni";
+month[6] = "Juli";
+month[7] = "Agustus";
+month[8] = "September";
+month[9] = "Oktober";
+month[10] = "November";
+month[11] = "Desember";
+
+function startTime() {
+
+  var today = new Date();
+  var hh = today.getHours();
+  var mm = today.getMinutes();
+  var ddd = weekday[today.getDay()]
+  var dd = today.getDate();
+  var mmm = month[today.getMonth()]
+  mm = checkTime(mm);
+  document.getElementById('mainClockTime').innerHTML =
+    hh + ":" + mm + "<br>" + ddd + ", " + dd + " " + mmm;
+  setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+  return i;
 }
