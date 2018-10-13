eec045b4feaf555c1a1b82532688b778a518a570
thomsetiabudi <thomsetiabudi@gmail.com>
2018-10-13 15:12:18 +0700

    done all komunitas
    done all komunitas

--- src/app/komunitas-premium/komunitas-premium.component.ts
+++ src/app/komunitas-premium/komunitas-premium.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModuleVerticalTitleComponent } from '../module-vertical-title/module-vertical-title.component';
+import { ModuleMenuComponent } from '../module-menu/module-menu.component';
+import { ModuleMenuLink } from '../module-menu-link';
 
 @Component({
   selector: 'app-komunitas-premium',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./komunitas-premium.component.scss']
 })
 export class KomunitasPremiumComponent implements OnInit {
+  @ViewChild('moduleTitleVertical') moduleTitleVertical: ModuleVerticalTitleComponent;
+  @ViewChild('moduleMenuLinkContainer') moduleMenuLinkContainer: ModuleMenuComponent;
+  @ViewChild('moduleContentContainer') moduleContentContainer: any;
+
+  currentOverlayMenu = 'komunitas';
+  moduleTitle = 'Komunitas';
+  moduleMenuLinkList: ModuleMenuLink[] = [
+    { title: 'Kids', routerLink: '/komunitas/kids', isActive: false, isSubMenu: false },
+    { title: 'Teens', routerLink: '/komunitas/teens', isActive: false, isSubMenu: false },
+    { title: 'Youth', routerLink: '/komunitas/youth', isActive: false, isSubMenu: false },
+    { title: 'Dewasa Muda', routerLink: '/komunitas/demud', isActive: false, isSubMenu: false },
+    { title: 'Excellent', routerLink: '/komunitas/excellent', isActive: false, isSubMenu: false },
+    { title: 'Premium', routerLink: '/komunitas/premium', isActive: true, isSubMenu: false }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onOverlayMenuActivated(activated: boolean) {
+    if (!activated) {
+      this.moduleMenuLinkContainer.toggleHideComponent(false);
+      this.moduleTitleVertical.toggleHideComponent(false);
+      (this.moduleContentContainer.nativeElement as HTMLElement).className = 'moduleContentContainer';
+    } else {
+      this.moduleMenuLinkContainer.toggleHideComponent(true);
+      this.moduleTitleVertical.toggleHideComponent(true);
+      (this.moduleContentContainer.nativeElement as HTMLElement).className = 'moduleContentContainer hideContent';
+    }
+  }
 }
