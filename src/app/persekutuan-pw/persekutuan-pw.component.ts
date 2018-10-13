227351c1d630f110b3534a6f3df1dda65a090a9e
thomsetiabudi <thomsetiabudi@gmail.com>
2018-10-13 16:04:06 +0700

    done all draft
    done all draft

--- src/app/persekutuan-pw/persekutuan-pw.component.ts
+++ src/app/persekutuan-pw/persekutuan-pw.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ModuleVerticalTitleComponent } from '../module-vertical-title/module-vertical-title.component';
+import { ModuleMenuComponent } from '../module-menu/module-menu.component';
+import { ModuleMenuLink } from '../module-menu-link';
 
 @Component({
   selector: 'app-persekutuan-pw',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persekutuan-pw.component.scss']
 })
 export class PersekutuanPwComponent implements OnInit {
+  @ViewChild('moduleTitleVertical') moduleTitleVertical: ModuleVerticalTitleComponent;
+  @ViewChild('moduleMenuLinkContainer') moduleMenuLinkContainer: ModuleMenuComponent;
+  @ViewChild('moduleContentContainer') moduleContentContainer: any;
+
+  currentOverlayMenu = 'persekutuan';
+  moduleTitle = 'Persekutuan';
+  moduleMenuLinkList: ModuleMenuLink[] = [
+    { title: 'Connect', routerLink: '/persekutuan/connect', isActive: true, isSubMenu: false },
+    { title: 'Persekutuan Wilayah', routerLink: '/komunitas/pw', isActive: false, isSubMenu: false }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onOverlayMenuActivated(activated: boolean) {
+    if (!activated) {
+      this.moduleMenuLinkContainer.toggleHideComponent(false);
+      this.moduleTitleVertical.toggleHideComponent(false);
+      (this.moduleContentContainer.nativeElement as HTMLElement).className = 'moduleContentContainer';
+     } else {
+      this.moduleMenuLinkContainer.toggleHideComponent(true);
+      this.moduleTitleVertical.toggleHideComponent(true);
+      (this.moduleContentContainer.nativeElement as HTMLElement).className = 'moduleContentContainer hideContent';
+     }
+  }
 }
